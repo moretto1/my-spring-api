882e4899de99f76a71c22795baf25bae7359cbaf
Bruno Moretto <bmoretto404@gmail.com>
2024-04-01 22:18:59 -0300

    Added to readme the project description

--- src/main/java/com/moretto/bruno/myspringapi/MySpringApiApplication.java
+++ src/main/java/com/moretto/bruno/myspringapi/MySpringApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MySpringApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MySpringApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringApiApplication.class, args);
+    }
 
 }
