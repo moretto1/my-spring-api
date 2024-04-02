2c17f8a8d86fe76af9825402469de54f670ffed4
Bruno Moretto <bmoretto404@gmail.com>
2024-04-01 21:46:47 -0300

    Init project

---
+++ src/test/java/com/moretto/bruno/myspringapi/MySpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moretto.bruno.myspringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
